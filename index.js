0104491d1d4caca214c6501276fa366e4f8a9149
hristog <hganev.apple@gmail.com>
2016-05-07 17:56:39 +0200

    add post for webhook to receive messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 // var pg = require('pg');
-var boryparser = require('bory-parser')
+var bodyparser = require('body-parser')
 // parse application/json
 app.use(bodyParser.json())
 
