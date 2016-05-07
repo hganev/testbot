cf6546f0615fecf9eb7a2760a095042d07dc46bb
hristog <hganev.apple@gmail.com>
2016-05-07 20:59:28 +0200

    add post for webhook to receive messages

--- index.js
+++ index.js
@@ -34,17 +34,23 @@ app.get('/webhook', function (req, res) {
   }
 });
 
+
+var allSenders = {};
 // to receive messages
 app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    var senderId = event.sender.id;
+    allSenders[senderId] = true;
     if (event.message && event.message.text) {
-      text = event.message.text;
+      var text = event.message.text;
       console.log(text);
+      console.log(Object.keys(allSenders));
+	  Object.keys(allSenders).forEach(function(senderId) {
+	  	sendTextMessage(senderId, "Text received, echo: "+ text.substring(0, 200));
+	  });
 
-	  sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
   }
   res.sendStatus(200);
